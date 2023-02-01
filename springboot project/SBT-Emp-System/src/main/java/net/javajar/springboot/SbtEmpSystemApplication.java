8178bee20f79b45e966fc137142a96a10ce064be
mukesh906 <82054670+mukesh906@users.noreply.github.com>
2023-02-01 14:27:10 +0530

    Add files via upload

---
+++ springboot project/SBT-Emp-System/src/main/java/net/javajar/springboot/SbtEmpSystemApplication.java
@@ -0,0 +1,13 @@
+package net.javajar.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtEmpSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtEmpSystemApplication.class, args);
+	}
+
+}
---
+++ springboot project/SBT-Emp-System/src/test/java/net/javajar/springboot/SbtEmpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javajar.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtEmpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
